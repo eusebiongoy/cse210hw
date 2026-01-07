cd50f11717eb28a7b29b4db3101ed3bb118ecefb
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-01-07 17:36:44 +0200

    exercise 1 week one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello World!");
+        int x;
+        x = 5;
+        Console.WriteLine(x);
+
+        int z = 7;
+        Console.WriteLine(z);
+
+        Console.Write("what is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"your favorite color is {color}");
     }
-}
+
+    
+} 
+
